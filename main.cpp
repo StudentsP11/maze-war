1fbecaf55311865efddd9c0752d7e461627b594e
toireymiuerthi <kamil.ismagilov.05@mail.ru>
2022-11-10 19:49:41 +0300

    3d test

--- main.cpp
+++ main.cpp
@@ -1,6 +1,37 @@
-#include <iostream>
+#include <GL/glew.h>
+#include <GL/GL.h>
+#include <GL/GLU.h>
+#include <GL/GLUT.h>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+void display();
+
+void init()
+{
+	glClearColor(0.0, 0.0, 0.0, 1);
+}
+
+int main(int argc, char **argv) {
+	glutInit(&argc, argv);
+	glutInitDisplayMode(0);
+
+	glutInitWindowPosition(200, 100);
+	glutInitWindowSize(500, 500);
+
+	glutCreateWindow("Window 1");
+
+	glutDisplayFunc(display);
+	glutReshapeFunc();
+	init();
+
+	glutMainLoop();
+
+	return 0;
+}
+
+void display()
+{
+	glClear(GL_COLOR_BUFFER_BIT);
+	glLoadIdentity();
+
+	glFlush();
 }
