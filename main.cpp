6019a06f0b58facd2f6ad545698f324ee0bd6eb7
Amir <105018355+sequze@users.noreply.github.com>
2022-11-15 19:28:42 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,37 @@
 #include <iostream>
+using namespace std;
+#include <cstdlib>
+#include <string>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+struct Point2D {
+	int x;
+	int y;
+};
+struct Point3D {
+	int x;
+	int y;
+	int z;
+};
+class GameObject {
+	
+};
+
+struct Texture {
+
+};
+class Entity{
+	string Name;
+	Point2D Position;
+	uint64_t Point3D;
+	int Direction;
+	int16_t Hp;
+};
+class Bonus {
+	Point2D;
+}
+
+
+
+int main()
+	baseEntity Player;
 }
