4c434b83e0e10ca46f3b124752a8cd51636052f4
T-i-m-u-r-PRO <117931613+T-i-m-u-r-PRO@users.noreply.github.com>
2022-11-15 19:41:35 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,39 @@
 #include <iostream>
+#include <conio.h>
+#include <windows.h>
+
+using namespace std;
+struct Point2D {
+    int x; 
+    int y;
+};
+struct Entity {
+    Point2D Position;
+};
+
+void MoveLeft(Entity &entity) {
+    entity.Position.x -= 1;
+
+}
+
+
+void MoveRight(Entity &entity) {
+    entity.Position.x += 1;
+}
+
+
+void MoveUp(Entity &entity) {
+    entity.Position.y += 1;
+
+}
+
+
+void MoveDown(Entity &entity) {
+    entity.Position.y -= 1;
+
+}
+
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+
 }
